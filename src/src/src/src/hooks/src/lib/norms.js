02b0321d83312715d8e6ec436e13a83e86808cdd
AppsFrees <tokimvest@gmail.com>
2026-03-22 17:18:50 -0500

    Create norms.js

---
+++ src/src/src/src/hooks/src/lib/norms.js
@@ -0,0 +1,8 @@
+// Normas básicas usadas en los cálculos
+
+export const norms = {
+  boltonAnterior: 77.2,   // % 6 ant.[web:82][web:85]
+  boltonTotal: 91.3,      // % 12 tot.[web:82][web:85]
+  tanakaSupOffset: 11.0,  // mm por cuadrante[web:87][web:89][web:91]
+  tanakaInfOffset: 10.5,  // mm por cuadrante[web:87][web:89][web:91]
+}
