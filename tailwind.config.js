1888d904f7adcf91c44f52e863c2ac5cd2730a42
AppsFrees <tokimvest@gmail.com>
2026-03-22 17:11:52 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50: '#f0fdfa', 100: '#ccfbf1
